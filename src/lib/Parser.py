33e8b48390f06a6be1a1e083bc483bffc755a117
johnbooch <johnbuccieri@gmail.com>
2018-12-23 14:21:34 -0500

    Added choices based on compression types available from shutil

--- src/lib/Parser.py
+++ src/lib/Parser.py
@@ -1,11 +1,13 @@
-from oauth2client import tools
+from shutil import get_archive_formats
 from argparse import ArgumentParser
 
+#from oauth2client import tools
+
 def build_drive_parser():
-    parser = ArgumentParser(description='Google Drive Backup CLI Parser', parents=[tools.argparser])
+    parser = ArgumentParser(description='Google Drive Backup CLI Parser')
     parser.add_argument('--source', type=str, default = None, help= 'Source folder or file to backup' )
     parser.add_argument('--destination', type=str, default="~", help='Destination for backup to be stored')
-    parser.add_argument('--compression', type=str, default= None, choices=['ZIP','TAR', 'GZ'], help='Compression format for downloaded files')
+    parser.add_argument('--compression', type=str, default= None, choices=[fmt[0] for fmt in get_archive_formats()], help='Compression format for downloaded files')
     parser.add_argument('--scope', type=str, default='READONLY', choices=['READONLY', 'FILE', 'DRIVE'], help='OAuth 2.0 scope (Must match OAuth Sceret File)')
     parser.add_argument('--folder', type=str, default='root', help='Folder to be backed up')
     parser.add_argument('--log_level', type=str, default="Info", help='Log' )
