81a651d720335ffe420a22c7b7591bf88445443d
johnbooch <johnbuccieri@gmail.com>
2018-12-22 23:16:47 -0500

    Added more command line options

--- src/lib/Parser.py
+++ src/lib/Parser.py
@@ -2,10 +2,11 @@ from oauth2client import tools
 from argparse import ArgumentParser
 
 def build_drive_parser():
-    parser = ArgumentParser(description="Google Drive Backup Parser", parents=[tools.argparser])
-    parser.add_argument('--destination', type=str, default="~", help='Destination for downloaded files')
-    parser.add_argument('--scope', type=str, default="READONLY", help="Oauth scope")
-    parser.add_argument('--compression', type=str, default=None, help="Compression format for downloaded files")
+    parser = ArgumentParser(description='Google Drive Backup CLI Parser', parents=[tools.argparser])
+    parser.add_argument('--source', type=str, default = None, help= 'Source folder or file to backup' )
+    parser.add_argument('--destination', type=str, default="~", help='Destination for backup to be stored')
+    parser.add_argument('--compression', type=str, default= None, choices=['ZIP','TAR', 'GZ'], help='Compression format for downloaded files')
+    parser.add_argument('--scope', type=str, default='READONLY', choices=['READONLY', 'FILE', 'DRIVE'], help='OAuth 2.0 scope (Must match OAuth Sceret File)')
     parser.add_argument('--folder', type=str, default='root', help='Folder to be backed up')
     parser.add_argument('--log_level', type=str, default="Info", help='Log' )
     
