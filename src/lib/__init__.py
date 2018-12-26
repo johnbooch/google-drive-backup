2be206009ded06e0adeab0449f7f228d21e6ef7a
johnbooch <johnbuccieri@gmail.com>
2018-12-25 22:33:24 -0500

    Added retry dec to lib

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -5,6 +5,8 @@ from lib import Utilities
 Parser = Parser
 Downloader = Downloader.Downloader
 
+RetryOnFailure = Utilities.RetryOnFailure
+
 GDRIVE_BACKUP_APP_DIR = Utilities.GDRIVE_BACKUP_APP_DIR
 GDRIVE_BACKUP_APP_AUTH_DIR = Utilities.GDRIVE_BACKUP_APP_AUTH_DIR
 GDRIVE_BACKUP_APP_LOG_DIR = Utilities.GDRIVE_BACKUP_APP_LOG_DIR
